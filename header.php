efb6eff78a3cab027fba224442b624731b3cf934
Jeppe Hasseriis <jehr@dr.dk>
2013-05-17 16:04:05 +0200

    ...

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+    <!-- Le styles -->
+    <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <div id="wrapper">
+    <div id="header">
+      <h1>HEADER</h1>
+    </div>
+    <div id="menu"></div>
+    <?php
+    wp_nav_menu(array(
+      'theme_location' => 'primary',
+      'sub_menu' => true
+    ));
+    ?>
