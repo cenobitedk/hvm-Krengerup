daa437a1a9c99c76673a64f08bb4ff53a1ca7560
Jeppe Hasseriis <j.hasseriis@gmail.com>
2013-08-16 00:03:50 +0200

    Changed footer to include specific page contents: slug=footer-page

--- footer.php
+++ footer.php
@@ -15,10 +15,18 @@
 
 <div class="footer-wrapper">
 	<footer id="colophon" role="contentinfo">
-		<div class="site-info">
-			
-			
-		</div><!-- .site-info -->
+	<?php
+		// query for the footer page 
+		$bottom_page_query = new WP_Query( 'pagename=footer-page' );
+
+		// loop through the query (even though it's just one page)
+		while ( $bottom_page_query->have_posts() ) : $bottom_page_query->the_post();
+		    the_content();
+		endwhile;
+
+		// reset post data (important, don't leave out!)
+		wp_reset_postdata();
+	?>
 	</footer><!-- #colophon -->
 </div>
 
