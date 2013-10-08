56493a9cbd28ae5eb01d81d2ec38f4d71bb6ce56
Jeppe Hasseriis <j.hasseriis@gmail.com>
2013-10-08 23:43:21 +0200

    Added footer widgets. Fixes #6

--- functions.php
+++ functions.php
@@ -231,6 +231,38 @@ function twentytwelve_widgets_init() {
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 	) );
+
+    // Register footer widgets
+    register_sidebar( array(
+        'name' => __( 'Footer Widget One', 'twentytwelve' ),
+        'id' => 'sidebar-4',
+        'description' => __( 'Found at the bottom of every page (except 404s and optional homepage template) Left Footer Widget.', 'twentytwelve' ),
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
+ 
+    register_sidebar( array(
+        'name' => __( 'Footer Widget Two', 'twentytwelve' ),
+        'id' => 'sidebar-5',
+        'description' => __( 'Found at the bottom of every page (except 404s and optional homepage template) Center Footer Widget.', 'twentytwelve' ),
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => "</aside>",
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
+ 
+    register_sidebar( array(
+        'name' => __( 'Footer Widget Three', 'twentytwelve' ),
+        'id' => 'sidebar-6',
+        'description' => __( 'Found at the bottom of every page (except 404s and optional homepage template) Right Footer Widget.', 'twentytwelve' ),
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => "</aside>",
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
+
 }
 add_action( 'widgets_init', 'twentytwelve_widgets_init' );
 
